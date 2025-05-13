11d5c3ace1f6ae7aca596ad424f5e297408764d1
jns-w <jns.w@icloud.com>
2025-05-13 10:17:42 +0800

    disable page for construction

--- app/blocks/page.tsx
+++ app/blocks/page.tsx
@@ -3,12 +3,13 @@ import { Suspense } from "react";
 
 export default function BlocksPage() {
 
-  return <main className="flex flex-col w-full items-center mt-[48px] mb-[64px]">
+  return <main className="flex flex-col w-full items-center mt-[48px] mb-[64px] min-h-[calc(100vh-300px)]">
     <div className="w-full max-w-[var(--max-width)] flex justify-center px-[var(--global-padding)]">
       <div className="flex flex-col justify-center gap-10 w-full">
-        <Suspense>
-          <BlocksTable />
-        </Suspense>
+        <p>This page is currently unavailable</p>
+        {/*<Suspense>*/}
+        {/*  <BlocksTable />*/}
+        {/*</Suspense>*/}
       </div>
     </div>
   </main>
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,12 +3,13 @@ import { Suspense } from "react";
 
 export default function TransactionsPage() {
 
-  return <main className="flex flex-col mt-[48px] mb-[64px] w-full items-center">
+  return <main className="flex flex-col mt-[48px] mb-[64px] w-full items-center min-h-[calc(100vh-300px)]">
     <div className="w-full max-w-[var(--max-width)] flex justify-center px-[var(--global-padding)]">
       <div className="flex flex-col justify-center gap-10 w-full">
-        <Suspense>
-          <TransactionsTable />
-        </Suspense>
+        <p>This page is currently unavailable</p>
+        {/*<Suspense>*/}
+        {/*  <TransactionsTable />*/}
+        {/*</Suspense>*/}
       </div>
     </div>
   </main>
